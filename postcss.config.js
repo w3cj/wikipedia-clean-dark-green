5d9bcf0c25e3930df31ec9faa268888ab6e700ad
Matthew Brush <mbrush@codebrainz.ca>
2022-09-24 15:30:32 -0700

    Switch from Vite to PostCSS CLI
    * Swaps vite dependency for postcss-cli & postcss-import
    * Removes need for dummy index.html
    * Doesn't generate superfluous JS files in dist directory
    * Leave package-lock.json unchanged as requested

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
+const postcssImport = require("postcss-import");
 const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
+    postcssImport(),
     postcssPresetEnv({ stage: 0 })
   ]
 }
